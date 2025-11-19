dc1585b1690ced7ab4b6035115c2ef88b091a650
Thawiat00 <Thawiat01@gmail.com>
2025-11-19 12:16:07 +0700

    comment on program.cs
    comment on program.cs

--- Razor_pages_101/Program.cs
+++ Razor_pages_101/Program.cs
@@ -1,15 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//add services to the container
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// cofig the http
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // the default hsts value is 30 day
     app.UseHsts();
 }
 
